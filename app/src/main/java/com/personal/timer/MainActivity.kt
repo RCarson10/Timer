d741fe90bf3fd3bbaea1f191d3cc25eb33f28c7c
Gaj Carson <gaj.carson@prepass.com>
2025-05-26 00:34:33 -0700

    First Iteration, added clock that displays the local timezone

--- app/src/main/java/com/personal/timer/MainActivity.kt
+++ app/src/main/java/com/personal/timer/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.personal.timer
 
 import android.os.Bundle
+import android.widget.TextClock
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import com.personal.timer.ui.theme.TimerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +30,54 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TimerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                DisplayTextClock()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun DisplayTextClock() {
+    Column(
+        modifier = Modifier.fillMaxSize()
+            .background(color = Color.Black),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Local Timezone Clock",
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            fontSize = 24.sp,
+            )
+        // Android view composable to use xml views in compose
+        AndroidView(
+            factory = { context ->
+
+                // Text Clock
+                TextClock(context).apply {
+
+                    // set hour format
+                    format12Hour = "hh:mm a"
+
+                    // set time zone
+                    timeZone = timeZone
+
+                    // set text size
+                    textSize= 32f
+
+                    // set text color
+                    setTextColor(ContextCompat.getColor(context, R.color.white))
+                }
+            },
+            modifier = Modifier.padding(5.dp),
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ClockPreview() {
     TimerTheme {
-        Greeting("Android")
+        DisplayTextClock()
     }
 }
